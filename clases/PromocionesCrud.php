974def96a83b495669f422ca3da210a9a06e0413
Adr1ni <adriano.gongora@tecsup.edu.pe>
2023-05-09 14:56:39 -0500

    borrando funcion que no nos sirve

--- clases/PromocionesCrud.php
+++ clases/PromocionesCrud.php
@@ -37,45 +37,5 @@ class PromocionesCrud extends Connection{
             return $th->getMessage();
         }
     }
-
-    public function promocionesDelClienteConJoin($id){
-        try{
-            $conexion = parent::connection();
-            $coleccion = $conexion->promociones;
-    
-            $datos = $coleccion->aggregate(array(
-                array(
-                    '$lookup' => array(
-                        'from' => 'personas',
-                        'localField' => '_id_cli',
-                        'foreignField' => '_id',
-                        'as' => 'per'
-                    )
-                ),
-                array(
-                    '$unwind' => '$per'
-                ),
-                array(
-                    '$project' => array(
-                        '_id' => 1,
-                        'per.nombre' => 1,
-                        'per.paterno' => 1,
-                        'promocion' => 1,
-                        'vencimiento' => 1,
-                        'per.numero' => 1
-                    )
-                ),
-                array(
-                    '$match' => array(
-                        '_id' => new MongoDB\BSON\ObjectId($id),
-                    )
-                )
-            ));
-            return $datos;
-    
-        }catch(\Throwable $th){
-            return $th->getMessage();
-        }
-    }
     
 }
